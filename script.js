9b4533c2ae3faf8fb4b265fcf43176c97b77cf96
Toki Tahmid Khan <122171015+toki513@users.noreply.github.com>
2024-04-06 14:20:52 +0600

    updated

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 const prompt = require("prompt-sync")();
 
-prompt("Enter something : ");
+let correctans = 0;
+
+console.log("Welcome to the Computer Hardware Quiz");
+
+const answer1 = prompt("What is the brain of the computer ? ");
+const correctAnswer1 = "CPU"
+
+if(answer1.toUpperCase() === correctAnswer1){
+    console.log("You got it correct ")
+    correctans++;
+}else{
+    console.log("You got it Wrong!!")
+}
+const answer2 = prompt("What is better a 3090ti or 4060 ? ");
+const correctAnswer2 = "3090TI"
+
+if(answer2.toUpperCase() === correctAnswer2){
+    console.log("You got it correct ")
+    correctans++;
+}else{
+    console.log("You got it Wrong!!")
+}
+
+const answer3 = prompt("What is the recommend amount of RAM in 2023 ? ");
+const correctAnswer3 = "16GB"
+
+if(answer3.toUpperCase() === correctAnswer3){
+    console.log("You got it correct ")
+    correctans++;
+}else{
+    console.log("You got it Wrong!!")
+}
+
+console.log("You got ",correctans, " correct answer.")
